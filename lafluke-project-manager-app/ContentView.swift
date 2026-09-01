a3a9934bb3daee36e10b6f2c51ed788f3a3c9044
Juan Cruz <astronaut@cruzantonio.net>
2026-08-31 20:38:37 -0700

    correct project name

---
+++ lafluke-project-manager-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lafluke-project-manager-app
+//
+//  Created by Juan Cruz on 8/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lafluke-project-manager-app/lafluke_project_manager_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  lafluke_project_manager_appApp.swift
+//  lafluke-project-manager-app
+//
+//  Created by Juan Cruz on 8/31/26.
+//
+
+import SwiftUI
+
+@main
+struct lafluke_project_manager_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
