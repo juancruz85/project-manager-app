94d4244194cec500c6bb79c612b755e59caa85cf
Juan Cruz <astronaut@cruzantonio.net>
2026-09-06 22:39:40 -0700

    bare bonea login page added

---
+++ lafluke-project-manager-app/LoginView.swift
@@ -0,0 +1,51 @@
+//
+//  LoginView.swift
+//  lafluke-project-manager-app
+//
+//  Created by Juan Cruz on 9/6/26.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    
+    
+    var body: some View {
+        ZStack {
+            Color(.pageBackground)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                Rectangle()
+                    .frame(height:56)
+                    .frame(maxWidth: .infinity)
+                
+                Spacer()
+                
+                VStack(spacing: 12) {
+                    Text("Sign in")
+                    Text("Manage your projects")
+
+                    TextField("Email", text: $email)
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(.roundedBorder)
+
+                    Button("Login") { }
+
+                    Text("NEW HERE")
+
+                    Button("Register") { }
+                }
+                .padding(20)
+                .frame(maxWidth: 384)
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
